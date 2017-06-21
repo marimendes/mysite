9888f079193c71957a7602a2601d445561dafd26
Mariana Mendes <mare.s.mendes@gmail.com>
2017-06-21 16:13:41 -0300

    Sending e-mail and create the comments

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,3 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'mysite.testando@gmail.com'
+EMAIL_HOST_PASSWORD = 'administrador'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
