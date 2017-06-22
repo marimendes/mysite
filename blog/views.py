d74dc1c61f90464d103e73245a591ee857fa0f7c
Mariana Mendes <mare.s.mendes@gmail.com>
2017-06-22 14:18:33 -0300

    Fixing the tag

--- blog/views.py
+++ blog/views.py
@@ -5,11 +5,19 @@ PageNotAnInteger
 from django.views.generic import ListView
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
+from taggit.models import Tag
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     object_list = Post.published.all()
+    tag = None
+
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+
     paginator = Paginator(object_list, 3) # 3 posts in each page
     page = request.GET.get('page')
+
     try:
         posts = paginator.page(page)
     except PageNotAnInteger:
@@ -18,10 +26,9 @@ def post_list(request):
     except EmptyPage:
         # If page is out of range deliver last page of results
         posts = paginator.page(paginator.num_pages)
-    return render(request,
-                    'blog/post/list.html',
-                    {'page': page,
-                    'posts': posts})
+    return render(request, 'blog/post/list.html', {'page': page,
+                                                    'posts': posts,
+                                                    'tag': tag})
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post,
--- mysite/settings.py
+++ mysite/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'taggit',
 ]
 
-MIDDLEWARE = [
+MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
